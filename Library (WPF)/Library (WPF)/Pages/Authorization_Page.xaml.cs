c6a3a5696e67b52cb19b6794a61275644583dab5
vZidv <94315999+vZidv@users.noreply.github.com>
2022-03-30 00:00:10 +0300

    ver 1.2

--- Library (WPF)/Library (WPF)/Pages/Authorization_Page.xaml.cs
+++ Library (WPF)/Library (WPF)/Pages/Authorization_Page.xaml.cs
@@ -43,8 +43,7 @@ namespace Library__WPF_.Pages
                 table.Clear();
 
                 SqlDataAdapter adapter = new SqlDataAdapter($"SELECT * FROM UserTable WHERE Login = '{login_Textbox.Text}' AND Password = '{password_Textbox.Text}'", sqlConnect.sqlCon);
-                adapter.Fill(table);
-                MessageBox.Show($"{login_Textbox.Text.Length}");
+                adapter.Fill(table);              
 
                 if (table.Rows.Count > 0)
                 {

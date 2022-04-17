a98b1cef1d68717edf6b736d69ac671e0ddb970f
vZidv <94315999+vZidv@users.noreply.github.com>
2022-04-17 23:35:31 +0300

    ver 1.1.8.4

--- Library (WPF)/Library (WPF)/Classes/SqlConnectClass.cs
+++ Library (WPF)/Library (WPF)/Classes/SqlConnectClass.cs
@@ -20,7 +20,7 @@ namespace Library__WPF_.Classes
 
         public  void SqlConnect()
         {
-            sqlCon = new SqlConnection(@"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = F:\Артём\Проекты и их материалы\Library(Wpf)\Project\Library-wpf-\Library (WPF)\Library (WPF)\Library.mdf; Integrated Security = True");
+            sqlCon = new SqlConnection(@"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = F:\Артём\Проекты и их материалы\Library(Wpf)\ProjectL\Library-wpf-\Library (WPF)\Library (WPF)\Library.mdf; Integrated Security = True");
             sqlCon.Open();
         }
         public void LoadTable(string command,DataGrid dataGrid)
